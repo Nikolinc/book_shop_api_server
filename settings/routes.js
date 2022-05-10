4e12cf7ff0e2ddf558d23ea5e09a2b88861ea832
Tsigulsky <tsigulskynikita@gmail.com>
2022-05-10 19:49:06 +0500

    add tag and slider

--- settings/routes.js
+++ settings/routes.js
@@ -4,8 +4,11 @@ module.exports = (app) => {
 
     const indexController = require('../controllers/indexController');
     const ProductController = require('../controllers/ProductController');
+    const TagFiltres = require('../controllers/TagFiltres');
+    const SliderController = require('../controllers/SliderController');
 
     app.route('/').get(indexController.index);
     app.route('/product').get(ProductController.product);
-
+    app.route('/tag').get(TagFiltres.TagFiltres);
+    app.route('/slider').get(SliderController.slider);
 }
