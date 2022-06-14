cf3f88118e7b8372c9e7da8df6eeb095c13c4bca
Nikolinc <tsigulskynikita@gmail.com>
2022-06-14 13:15:53 +0500

    add docs on roles

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,4 +1,9 @@
-export class CreateRoleDto{
-  readonly value:string
-  readonly description:string;
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateRoleDto {
+  @ApiProperty({ example: 'USER', description: 'value role' })
+  readonly value: string;
+
+  @ApiProperty({ example: 'user role ', description: 'description role' })
+  readonly description: string;
 }
--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,16 +1,19 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { RolesService } from './roles.service';
 
+@ApiTags('Roles')
 @Controller('roles')
 export class RolesController {
   constructor(private roleService: RolesService) {}
 
+  @ApiOperation({ summary: 'Create roles' })
   @Post()
   create(@Body() dto: CreateRoleDto) {
     return this.roleService.createRole(dto);
   }
-
+  @ApiOperation({ summary: 'get array role' })
   @Get('/value')
   getByValue(@Param('value') value: string) {
     return this.roleService.getRoleByValue(value);
